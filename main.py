f7bd214822b38b7dcbda5030ff8c751cdba6e4aa
SpecteriuS <91021847+SpecteriuS@users.noreply.github.com>
2021-12-06 15:00:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -25,38 +25,39 @@ def answer(message):
     if message.text.lower() == "хочу":
         bot.send_message(message.chat.id, 'Тогда тебе сюда – https://mtuci.ru/')
     if message.text.lower() == "понедельник":
-        bot.send_message(message.chat.id, 'Верхняя и Нижняя недели')
-        bot.send_message(message.chat.id, 'ВВИТ практика 9:30-11:05')
-        bot.send_message(message.chat.id, 'КГ практ 11:20-12:55')
-        bot.send_message(message.chat.id, 'Философия практ 13:10-14:45')
-        bot.send_message(message.chat.id, 'Выч.Тех практ и лаб 15:25-17:00')
+        bot.send_message(message.chat.id, 'Верхняя и Нижняя недели'
+                                            '\nВВИТ практика 9:30-11:05'
+                                            '\nКГ практ 11:20-12:55'
+                                            '\nФилософия практ 13:10-14:45'
+                                            '\nВыч.Тех практ и лаб 15:25-17:00')
     if message.text.lower() == "вторник":
         bot.send_message(message.chat.id, 'Верхняя неделя = ничего')
-        bot.send_message(message.chat.id, 'Нижняя неделя')
-        bot.send_message(message.chat.id, 'ВВит лаб 13:10-14:45')
-        bot.send_message(message.chat.id, 'ВВит лаб 15:25-17:00')
-        bot.send_message(message.chat.id, 'ВВит лекц 17:15-18:50')
+        bot.send_message(message.chat.id, 'Нижняя неделя'
+                                              '\nВВит лаб 13:10-14:45'
+                                              '\nВВит лаб 15:25-17:00'
+                                              '\nВВит лекц 17:15-18:50')
     if message.text.lower() == "среда":
-        bot.send_message(message.chat.id, 'Нижняя неделя')
-        bot.send_message(message.chat.id, 'Ин яз 9:30-11:05')
-        bot.send_message(message.chat.id, 'Ин яз 11:20-12:55')
-        bot.send_message(message.chat.id, 'Верхняя неделя')
-        bot.send_message(message.chat.id, 'Философия лекция 13:10-14:45')
-        bot.send_message(message.chat.id, 'Кг лекция 15:25-17:00')
+        bot.send_message(message.chat.id, 'Нижняя неделя'
+                                            '\nИн яз 9:30-11:05'
+                                            '\nИн яз 11:20-12:55')
+        bot.send_message(message.chat.id, 'Верхняя неделя'
+                                              '\nФилософия лекция 13:10-14:45'
+                                              '\nКг лекция 15:25-17:00')
     if message.text.lower() == "четверг":
-        bot.send_message(message.chat.id, 'Нижняя неделя')
-        bot.send_message(message.chat.id, 'Информационная экология лекц 11:20-12:55')
-        bot.send_message(message.chat.id, 'Информационная экология практика 15:25-17:00')
-        bot.send_message(message.chat.id, 'Верхняя неделя')
-        bot.send_message(message.chat.id, 'Алгебра и Геометрия лекц 15:25-17:00')
-        bot.send_message(message.chat.id, 'Выш.мат лекц 17:15-18:50')
+        bot.send_message(message.chat.id, 'Нижняя неделя'
+                                            '\nИнформационная экология лекц 11:20-12:55'
+                                            '\nИнформационная экология практика 15:25-17:00')
+        bot.send_message(message.chat.id, 'Верхняя неделя'
+                                            '\nАлгебра и Геометрия лекц 15:25-17:00'
+                                            '\nВыш.мат лекц 17:15-18:50')
     if message.text.lower() == "пятница":
-        bot.send_message(message.chat.id, 'Верхняя и Нижняя недели')
-        bot.send_message(message.chat.id, 'Выч.тех лекц 11:20-12:55')
+        bot.send_message(message.chat.id, 'Верхняя и Нижняя недели'
+                                           '\nВыч.тех лекц 11:20-12:55')
     if message.text.lower() == "суббота":
-        bot.send_message(message.chat.id, 'Верхняя и Нижняя недели')
-        bot.send_message(message.chat.id, 'Выш.мат практика 9:30-11:05')
-        bot.send_message(message.chat.id, 'Алгебра и Геометрия практика 11:20-12:55')
+        bot.send_message(message.chat.id, 'Верхняя и Нижняя недели'
+                                            '\nВыш.мат практика 9:30-11:05'
+                                            '\nАлгебра и Геометрия практика 11:20-12:55')
+
 
 
 
